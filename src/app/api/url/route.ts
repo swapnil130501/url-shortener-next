c4662d9e5efd07a5f11c94e084a94a6a8bcae71c
swapnil <swapnil130501@gmail.com>
2025-01-03 14:15:29 +0530

    update create shorten url api

--- src/app/api/url/route.ts
+++ src/app/api/url/route.ts
@@ -10,6 +10,14 @@ export async function POST(req: Request) {
         const body = await req.json();
         const { url } = body;
 
+        if (!url) {
+            return NextResponse.json({
+                status: 400,
+                success: false,
+                message: "url is required",
+            });
+        }
+
         const shortId = Math.random().toString(36).substring(2, 8);
         const payload = {
             shortId: shortId,
@@ -17,7 +25,12 @@ export async function POST(req: Request) {
             visitHistory: [],
         };
         const result = await URL.create(payload);
-        return NextResponse.json(result, { status: 201 });
+        return NextResponse.json({
+            status: 201,
+            success: true,
+            message: "URL shortened successfully",
+            data: result
+        });
     } catch (error) {
         console.error("Error creating short URL:", error);
         return NextResponse.json({ error: "Internal Server Error" }, { status: 500 });
